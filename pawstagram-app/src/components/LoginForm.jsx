1a2cc908ff3bfa15cd33c63c7861f941d2359c88
Doreen <doreenmarie_cee@hotmail.com>
2024-01-09 00:10:30 +0100

    cleaning up merge conflicts

--- pawstagram-app/src/components/LoginForm.jsx
+++ pawstagram-app/src/components/LoginForm.jsx
@@ -45,7 +45,7 @@ function LoginForm({ onLoginSuccess }){
     }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
+    <div className='min-h screen flex items-center justify-center bg-white'>
     <div className="max-w-md w-full space-y-8 p-10 bg-teal-200 rounded-xl shadow-lg z-10">
         <div className="text-center">
           <h2 className="mt-6 text-3xl font-bold text-gray-900">
