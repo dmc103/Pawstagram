a5c9a070a2e45e6139a6b72c6317bf2120f2aa88
Alkim <alkim-atanasov@outlook.com>
2024-01-08 22:47:35 +0100

    tailwind.css codes added

--- pawstagram-app/components/LoginForm.jsx
+++ pawstagram-app/components/LoginForm.jsx
@@ -23,15 +23,15 @@ function LoginForm({ onLoginSuccess }){
       };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl shadow-lg z-10">
+    <div className="min-h-screen flex items-center justify-center bg-blue-200">
+      <div className="max-w-md w-full space-y-8 p-10 bg-red-50 rounded-xl shadow-lg z-10">
         <div className="text-center">
-          <h2 className="mt-6 text-3xl font-bold text-gray-900">
+          <h2 className="italic text-3xl font-bold text-blue-900">
             WELCOME BACK
           </h2>
           <br />
-          <p className="mt-2 text-sm text-gray-600">
-            you need to Log In first
+          <p className="italic text-blue-600">
+            You need to Log in first !
           </p>
           <div>
           <img src={logo} alt="Pawstagram logo" className="mx-auto w-32 sm:w-24 md:w-20 lg:w-16 h-auto"/>
@@ -41,10 +41,11 @@ function LoginForm({ onLoginSuccess }){
 
         </div>
         <form className="space-y-6" onSubmit={handleSubmit}>
+      
           <input
             type="email"
             name="email"
-            className="w-full p-3 border border-gray-300 rounded-md"
+            className="w-full p-3 border border-gray-400 rounded-md"
             placeholder="Username or Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -54,7 +55,7 @@ function LoginForm({ onLoginSuccess }){
           <input
             type="password"
             name="password"
-            className="w-full p-3 border border-gray-300 rounded-md"
+            className="w-full p-3 border border-gray-700 rounded-md bg-sky-500 hover:bg-sky-700"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -65,28 +66,28 @@ function LoginForm({ onLoginSuccess }){
 
           <div className="flex items-center justify-between">
             <div className="flex items-center">
-              <input id="remember_me" name="remember_me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
-              <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+              <input id="remember_me" name="remember_me" type="checkbox" className="h-4 w-4 text-purple-500 focus:ring-indigo-600 border-gray-400 rounded"/>
+              <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900 hover:text-indigo-500">
                 Remember me
               </label>
             </div>
             <div className="text-sm">
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+              <a href="#" className="font-medium text-teal-200 hover:text-indigo-500 focus-ring-1">
                 Forgot your password?
               </a>
             </div>
           </div>
           <button
             type="submit"
-            className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
+            className="w-full flex justify-center bg-blue-700 hover:bg-blue-800 text-white-w-full py-2 px-3 rounded focus-ring-1"
           >
             Log in
           </button>
         </form>
         <div className="text-center">
-          <p className="mt-2 text-sm text-gray-600">
+          <p className="mt-2 text-sm text-teal-200">
           Don&apos;t have an account?{' '}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+            <a href="#" className="font-medium text-teal-200 hover:text-indigo-600">
               Register
             </a>
           </p>
