0c1aa1e149988cb2c5fa4d44d09c06c04bb767f9
Doreen <doreenmarie_cee@hotmail.com>
2024-01-08 22:21:07 +0100

    applying some css

--- pawstagram-app/src/components/RegisterPage.jsx
+++ pawstagram-app/src/components/RegisterPage.jsx
@@ -62,7 +62,7 @@ function RegisterPage() {
     <div className="min-h-screen flex items-center justify-center bg-white">
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-indigo-900">
           Welcome
         </h2>
         <p className="mt-2 text-center text-sm text-indigo-700">{registerSuccess ? 'REGISTRATION SUCCESSFUL' : 'PLEASE REGISTER HERE'}</p>
